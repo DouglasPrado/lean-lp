3c57c2b325893eb9067b647c7691f429e1d9028d
Douglas Prado <douglasrafaeldoprado@gmail.com>
2026-02-26 09:26:01 -0300

    add novas linguagens

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -1,6 +1,6 @@
 import { defineRouting } from "next-intl/routing"
 
 export const routing = defineRouting({
-  locales: ["pt-BR", "en"],
-  defaultLocale: "pt-BR",
+  locales: ["en", "pt-BR", "zh", "hi", "es", "fr"],
+  defaultLocale: "en",
 })
